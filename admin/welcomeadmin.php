65d7443ceb6bd39a13305b6fd495e7151f080742
Aung Phone Myat <aungphonemyat@Aungs-MacBook-Pro.local>
2022-07-20 16:33:03 +0630

    Init

---
+++ admin/welcomeadmin.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['user_id'])){
+	header('Location: index.php');
+}
+
+
+
+echo $_SESSION['user_id'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+</head>
+<body>
+	<h1> Hi admin</h1>
+	<a href="logout.php">Logout</a>
+</body>
+</html>
