0b914b7d3e3f0a77803cc91edda11361569c6b9b
Aaronkst <aaron.kst99@gmail.com>
2022-07-20 16:05:18 +0630

    fix locations

--- index.php
+++ index.php
@@ -5,22 +5,22 @@ include '../config.php';
 session_start();
 error_reporting(0);
 
-if(isset($_SESSION['user_id'])){
-  header("Location: welcomeadmin.php");
+if (isset($_SESSION['user_id'])) {
+  header("Location: add.php");
 }
 
 
 if (isset($_POST["signin"])) {
-  $email = mysqli_real_escape_string($conn,$_POST["email"]);
-  $password = mysqli_real_escape_string($conn,md5($_POST["password"]));
+  $email = mysqli_real_escape_string($conn, $_POST["email"]);
+  $password = mysqli_real_escape_string($conn, md5($_POST["password"]));
 
   $check_email = mysqli_query($conn, "SELECT id FROM users WHERE email='$email' AND password='$password'");
 
-  if(mysqli_num_rows($check_email) > 0 && $email === "admin@gmail.com"){
-    $row=mysqli_fetch_assoc($check_email);
-    $_SESSION['user_id']=$row['id'];
-    header("Location: welcomeadmin.php");
-  }else{
+  if (mysqli_num_rows($check_email) > 0 && $email === "admin@gmail.com") {
+    $row = mysqli_fetch_assoc($check_email);
+    $_SESSION['user_id'] = $row['id'];
+    header("Location: add.php");
+  } else {
     echo "<script>alert('Email or password is incorrect')</script>";
   }
 }
@@ -48,7 +48,7 @@ if (isset($_POST["signin"])) {
           </div>
           <div class="input-field">
             <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Password" name="password" value="<?php echo $_POST["password"] ?>"/>
+            <input type="password" placeholder="Password" name="password" value="<?php echo $_POST["password"] ?>" />
           </div>
           <input type="submit" value="Login" name="signin" class="btn solid" />
           <p style="display: flex;justify-content: center;align-items: center;margin-top: 20px;"><a href="forgot-password.php" style="color: #4590ef;">Forgot Password?</a></p>
@@ -67,7 +67,7 @@ if (isset($_POST["signin"])) {
   </div>
 
   <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
- 
+
 </body>
 
 </html>
--- welcomeadmin.php
+++ welcomeadmin.php
@@ -2,11 +2,9 @@
 
 session_start();
 
-if(!isset($_SESSION['user_id'])){
+if (!isset($_SESSION['user_id'])) {
 	header('Location: index.php');
 }
-
-
 
 echo $_SESSION['user_id'];
 
@@ -14,13 +12,16 @@ echo $_SESSION['user_id'];
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Document</title>
 </head>
+
 <body>
 	<h1> Hi admin</h1>
 	<a href="logout.php">Logout</a>
 </body>
+
 </html>
